f2ffacd819708e105980f8246398f10983076e22
AhmedBelal15 <62512711+AhmedBelal15@users.noreply.github.com>
2020-11-02 03:16:01 +0200

    trying to fix something

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ const updatePassword = require('./routes/updatePassword')
 const markTodo = require('./routes/markTodo')
 
 //route middlewares
+app.use("/", (req,res)=> res.send('we are online'));
 app.use("/api/user", registerAuth);
 app.use("/api/user", loginAuth);
 app.use("/api/user", createTodo);
